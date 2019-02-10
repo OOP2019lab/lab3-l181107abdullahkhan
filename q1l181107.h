f9e87b3e18716b0f9562b921530ea10f0795754a
l181107abdullahkhan <46881904+l181107abdullahkhan@users.noreply.github.com>
2019-02-10 17:20:03 +0500

    Add files via upload

---
+++ q1l181107.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ 
+struct githubuser
+{
+	string firstname;
+	string username;
+	string email;
+	int filecount;
+	string *githubfolders;
+	string *institute_name;
+	string *qual_lvl;
+};
